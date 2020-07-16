f6d67bacc1b805f285ea70b8080f74d93c916257
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-16 15:07:26 -0500

    Adds current work

--- src/adv.py
+++ src/adv.py
@@ -36,9 +36,7 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
-
 # Make a new player object that is currently in the 'outside' room.
-
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +47,31 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
+
+from player import Player
+
+current_room = 'outside'
+game_over = False
+player = Player(room[current_room])
+cardinals = ['n', 's', 'w', 'e'] # remove this if not needed in memory, used in elif under while not game_over
+
+while not game_over:
+    print(player.current_room)
+    command = input("Choose wisely:\nEnter the first letter of a cardinal direction, or 'q' to quit: ").lower()
+    if command == 'q':
+        game_over = not game_over
+        print("\nYou have asked to quit the game")
+        break
+    elif any([direction == command for direction in cardinals]):
+        dir_key = f'{command}_to'
+        for key in room[current_room].__dict__:
+            if key == dir_key:
+                print(f'key: {key}\ncurrent_room: {room[current_room]}\ndir_key: {dir_key}')
+                print(f'building out: {room}')
+    else:
+        print(f"\n{command} isn't a recognized command. Enter the first letter of a cardinal direction or 'q'")
+
+if game_over:
+    print("Game Over")
--- src/room.py
+++ src/room.py
@@ -1,9 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+import textwrap
 class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
 
     def __str__(self):
-        return f'{name} room is {description}'
+        desc = "\n".join(textwrap.wrap(self.description))
+        return f'{self.name} room is {desc}'
