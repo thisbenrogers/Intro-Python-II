06e0feeaa0ec76ef45fe56ceed637a16d992a7c3
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-20 21:07:06 -0500

    Updates Player Class to allow carrying item

--- src/player.py
+++ src/player.py
@@ -1,9 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player():
-    def __init__(self, current_room):
+    def __init__(self, current_room, carrying=None):
+        if carrying is None:
+            carrying = []
         self.current_room = current_room
         
 
     def __str__(self):
-        return f'Player location: {self.current_room}'
+        return f'Player location: {self.current_room}, carrying items: {self.carrying}'
